a5e6537cc1182077eda30e643eb6a5187e068ee1
yangrui <yweiyun@gmail.com>
2015-09-24 20:09:11 +0800

    extend Singleton for heap alloc

--- cpp/Singleton.h
+++ cpp/Singleton.h
@@ -1,35 +1,37 @@
 #ifndef _SINGLETON_H_
 #define _SINGLETON_H_
 #include <cassert>
-// From CEGUI
-/* Usage
- class LoadBalancer : private Singleton<LoadBalancer>
- {
-
- };
- LoadBalancer::get_instance();
- */
-template <typename T> class Singleton
+#include <stdio.h>
+template <typename T, bool heap_alloc = true> class Singleton
 {
 protected:
     static T* m_inst_;
 public:
     Singleton()
     {
-        assert( !m_inst_ );
-        m_inst_ = static_cast<T*>(this);
+        if (!heap_alloc)
+        {
+            assert( !m_inst_ );
+            m_inst_ = static_cast<T*>(this);
+        }
     }
-   ~Singleton()
-   {
-     assert( m_inst_ );
-     m_inst_ = 0;
-   }
-   static T* get_instance()
-   {
-     return ( m_inst_ );
-   }
+    ~Singleton()
+    {
+        assert( m_inst_ );
+        m_inst_ = 0;
+    }
+    static T* instance()
+    {
+        if (heap_alloc && !m_inst_)
+        {
+            m_inst_ = new T();
+        }
+        return ( m_inst_ );
+    }
 private:
     Singleton& operator=(const Singleton&);
     Singleton(const Singleton&);
 };
-#endif	// _SINGLETON_H_
+template <typename T, bool heap_alloc>
+T* Singleton<T, heap_alloc>::m_inst_ = NULL;
+#endif  // _SINGLETON_H_
